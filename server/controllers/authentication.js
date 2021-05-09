8842c73a8ed54a0e90037ad9f1609d3a53d43cf2
Roberto-Livi <robertomlivi@outlook.com>
2021-05-09 13:47:28 -0500

    searching for users using findOne

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,11 @@
 exports.signup = function(req, res, next) {
+    console.log(req.body)
+    // See if a user with the given email exists
+
+    // if a user with email does exist, return an error
+
+    // if a suer with email does NOT exist, create and save user record
+
+    // Respond to request indicating the user was created
     res.send({ success: true });
 }
